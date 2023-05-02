f74dae35898fcb06dfba0eac15ea3b65620d377c
Dusan S <dusan.stojanovicc@elfak.rs>
2023-05-02 23:10:14 +0200

    dodato par komponenti,nije dobro radilo

--- Aplikacija/fitnessivity/src/app/app.module.ts
+++ Aplikacija/fitnessivity/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MainPageComponent,
+    FooterComponent,
+
   ],
   imports: [
     BrowserModule,
