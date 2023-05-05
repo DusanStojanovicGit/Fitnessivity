7fedea2025959c99b388d5de24bde4aad45ce3c3
maridius <maridius@elfak.rs>
2023-05-05 21:04:15 +0200

    routing

--- Aplikacija/fitnessivity/src/app/app-routing.module.ts
+++ Aplikacija/fitnessivity/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PlansPageComponent } from './plans-page/plans-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'plans', component: PlansPageComponent},
+  { path: 'login', component: LoginPageComponent},
+  { path: 'register', component: RegisterPageComponent},
 ];
 
 @NgModule({
