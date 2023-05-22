e2619c56bb6660975f143eeeb1dfc8c1db38768c
Dusan S <dusan.stojanovicc@elfak.rs>
2023-05-22 16:34:18 +0200

    login salje podatek v1

--- Aplikacija/fitnessivity/src/app/login-page/login-page.component.ts
+++ Aplikacija/fitnessivity/src/app/login-page/login-page.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class LoginPageComponent {
 
+  constructor(private http: HttpClient){
+
+  }
+
+  OnLogin(account: {email: string, password: string}){
+    console.log(account);
+    this.http.post('http://127.0.0.1:3000/user/login', account)
+    .subscribe((res)=>{
+      console.log(res);
+    });
+  }
 }
