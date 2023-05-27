0d1cdedb6d49faa79e3b2722f5223383c22e126b
Dusan S <dusan.stojanovicc@elfak.rs>
2023-05-27 17:04:16 +0200

    komit spajanja

--- Aplikacija/fitnessivity/src/app/register-page/register-page.component.ts
+++ Aplikacija/fitnessivity/src/app/register-page/register-page.component.ts
@@ -1,22 +1,61 @@
+import { MatchPassword } from './validators/match-password';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.css']
+  styleUrls: ['./register-page.component.css'],
 })
-export class RegisterPageComponent {
+export class RegisterPageComponent implements OnInit {
+  regForm = new FormGroup(
+    {
+      username: new FormGroup('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+        Validators.pattern(/^[a-z0-9]+$/),
+      ]),
 
-  constructor(private http: HttpClient){
+      fullName: new FormGroup('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20),
+        Validators.pattern(/^[A-Za-z]+$/),
+      ]),
 
+      email: new FormGroup('', [Validators.email, Validators.required]),
+      password: new FormGroup('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20),
+      ]),
+
+      confirmPassword: new FormGroup('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20),
+      ]),
+    },
+    { validators: [this.matchPassword.validate] }
+  );
+
+  constructor(private http: HttpClient, private matchPassword: MatchPassword) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
-  onAccountCreate(account: {name: string, username: string, email: string, password: string}){
+  onAccountCreate(account: {
+    name: string;
+    username: string;
+    email: string;
+    password: string;
+  }) {
     console.log(account);
-    this.http.post('http://127.0.0.1:3000/user/register', account)
-    .subscribe((res)=>{
-      console.log(res);
-    });
+    this.http
+      .post('http://127.0.0.1:3000/user/register', account)
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
 }
---
+++ Aplikacija/fitnessivity/src/app/register-page/validators/match-password.ts
@@ -0,0 +1,20 @@
+import {
+  AbstractControl,
+  FormGroup,
+  ValidationErrors,
+  Validator,
+} from '@angular/forms';
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class MatchPassword implements Validator {
+  validate(control: AbstractControl): ValidationErrors | null {
+    const { password, confirmPassword } = control.value;
+
+    if (password === confirmPassword) {
+      return null;
+    } else {
+      return { passwordsDontMatch: true };
+    }
+  }
+}
