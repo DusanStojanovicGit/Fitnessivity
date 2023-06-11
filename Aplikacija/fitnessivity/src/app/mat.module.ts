47c3a5e5d9d0b769edd6a309b78406c54c62deee
maridius <maridius@elfak.rs>
2023-06-11 13:45:28 +0200

    plan-page-view

--- Aplikacija/fitnessivity/src/app/mat.module.ts
+++ Aplikacija/fitnessivity/src/app/mat.module.ts
@@ -6,9 +6,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDialogModule} from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion'
 
 @NgModule({
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatChipsModule, MatIconModule, MatDialogModule, MatButtonModule],
-  exports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatChipsModule, MatIconModule, MatDialogModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatChipsModule, MatIconModule, MatDialogModule, MatButtonModule, MatExpansionModule],
+  exports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatChipsModule, MatIconModule, MatDialogModule, MatButtonModule, MatExpansionModule],
 })
 export class MaterialModule {}
