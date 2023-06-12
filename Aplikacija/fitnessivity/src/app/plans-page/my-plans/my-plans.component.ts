d8a4d1e9706de6072699e8d96555a7057752dcf6
Dusan S <dusan.stojanovicc@elfak.rs>
2023-06-12 14:21:35 +0200

    poruka

--- Aplikacija/fitnessivity/src/app/plans-page/my-plans/my-plans.component.ts
+++ Aplikacija/fitnessivity/src/app/plans-page/my-plans/my-plans.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Plan } from 'src/app/plan/plan.entity';
+import { PlanService } from 'src/app/plan/plan.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-my-plans',
   templateUrl: './my-plans.component.html',
-  styleUrls: ['./my-plans.component.css']
+  styleUrls: ['./my-plans.component.css'],
 })
 export class MyPlansComponent {
+  plans$!: Observable<Plan[]>;
+  selectedGenre: string = '';
 
+  constructor(private planService: PlanService, private route: ActivatedRoute) {
+    this.route.queryParams.subscribe((params) => {
+      this.selectedGenre = params['genre'];
+    });
+  }
+
+  ngOnInit() {}
 }
